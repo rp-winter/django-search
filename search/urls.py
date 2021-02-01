1a8fea9583f2abaed51c03c770fb4483412e303e
Ritik Pal <rp-winter>
2021-02-01 14:15:07 +0530

    new features

--- search/urls.py
+++ search/urls.py
@@ -5,6 +5,7 @@ app_name = 'search'
 urlpatterns = [
     path('', views.index, name='home'),
     path('search_institute/', views.search_institute, name = 'search_institute'),
+    path('validate_password1/', views.validate_password1, name = 'validate_password1'),
     path('submit/', views.submit, name = 'submit'),
     path('print_form/', views.print_form, name = 'print_form'),
 ]
--- search/views.py
+++ search/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Institute, Users
 from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+import re
+from difflib import SequenceMatcher
 
 # Create your views here.
 
@@ -12,12 +16,37 @@ def index(request):
 def search_institute(request):
     q = request.POST['institution']
     if q:
-        object_list = Institute.objects.filter(name__icontains=q)
+        object_list = Institute.objects.filter(name__istartswith=q)
         html = ""
         for object in object_list:
             html = html + '<div onclick="change_value(this)">'+object.name+'</div>'
     else:
         html = ""
+    return HttpResponse(html)
+
+def validate_password1(request):
+    password1 = request.POST['password1']
+    username = request.POST['username']
+    fname = request.POST['first_name']
+    lname = request.POST['last_name']
+    email = request.POST['email']
+    html = ""
+    user_attribute = (username, fname, lname, email)
+    similar = False
+    for value in user_attribute:
+        if not value or not isinstance(value, str):
+                continue
+        value_parts = re.split(r'\W+', value) + [value]
+        for value_part in value_parts:
+            if SequenceMatcher(a=password1.lower(), b=value_part.lower()).quick_ratio() >= 0.7:
+                similar = True
+    if similar:
+        html = html + '<li>Your password can’t be too similar to your other personal information.</li>'
+    try:
+        validate_password(password1)
+    except ValidationError as errors:
+        for error in errors:
+            html = html + '<li>'+error+'</li>'
     return HttpResponse(html)
 
 def print_form(request):
